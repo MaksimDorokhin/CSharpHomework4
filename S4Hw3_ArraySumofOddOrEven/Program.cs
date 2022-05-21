9ca82f85eb35c56b5e50062ce1c3d9871b887991
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-05-21 14:55:12 +0300

    Added S4Hw3 task: Qty of even/odd elements of array

---
+++ S4Hw3_ArraySumofOddOrEven/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. 
+// Показать количество нечетных\четных чисел.
+Console.Clear();
+int[] arr = new int[8];
+int countEven = 0;
+int countOdd = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(100,1000); 
+    Console.Write($"{arr[i]} ");
+    if (arr[i] % 2 == 0) countEven++;
+    else countOdd++;
+}
+Console.WriteLine();
+Console.WriteLine($"Количество четных элементов массива равно {countEven}!");
+Console.WriteLine($"Количество нечетных элементов массива равно {countOdd}!");
