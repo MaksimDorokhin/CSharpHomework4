d865780250ee815f5da38537137284f083130467
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-05-21 15:50:47 +0300

    Formatted S4Hw5

--- S4Hw5_ProductOfPairsInArray/Program.cs
+++ S4Hw5_ProductOfPairsInArray/Program.cs
@@ -2,19 +2,19 @@
 // Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 Console.Clear();
 Console.Write("Введите кол-во элементов одномерного массива: ");
-int length = int.Parse(Console.ReadLine() ?? "0"); 
+int length = int.Parse(Console.ReadLine() ?? "0");
 int[] arr = new int[length];
-int[] result = new int[length/2];
+int[] result = new int[length / 2];
 for (int i = 0; i < length; i++)
 {
-    arr[i] = new Random().Next(-9,10); 
+    arr[i] = new Random().Next(-9, 10);
     Console.Write($"{arr[i]} ");
 }
 Console.WriteLine();
 Console.WriteLine("Произведения пар чисел:");
-for (int i = 0; i < length / 2; i++) 
+for (int i = 0; i < length / 2; i++)
 {
-    result[i] = arr[i] * arr[length - 1  - i];
+    result[i] = arr[i] * arr[length - 1 - i];
     Console.Write($"{result[i]} ");
 }
 Console.WriteLine();
