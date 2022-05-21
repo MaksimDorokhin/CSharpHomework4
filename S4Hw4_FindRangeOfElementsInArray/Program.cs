bf5f9552347e36a9865169211469484d5326476e
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-05-21 15:19:38 +0300

    Added S4Hw4 task: Qty of elemets from 10 to 99 in array of 123 elements

---
+++ S4Hw4_FindRangeOfElementsInArray/Program.cs
@@ -0,0 +1,14 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99].
+Console.Clear();
+int[] arr = new int[123];
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(-1000,1001); 
+    Console.Write($"{arr[i]} ");
+    if (arr[i] >= 10 && arr[i] <= 99) count++;
+}
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine($"Количество элементов массива из отрезка [10,99] равно {count}!");
+Console.WriteLine();
